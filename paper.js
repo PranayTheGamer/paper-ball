8d42883950cc58c6c35f818e997c8e768b4ca6a8
PranayTheGamer <74360649+PranayTheGamer@users.noreply.github.com>
2021-01-04 11:35:47 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,23 @@
+class Paper {
+    constructor(x, y, radius) {
+      var options = {
+          'isStatic': false,
+          'restitution':0.3,
+          'friction':0.5,    
+          'density':1.2
+      }
+      this.body = Bodies.circle(x, y, radius, options);
+      this.radius = radius;
+      this.image = loadImage("paperball-noshadow-min.png");
+      World.add(world, this.body);
+    }
+    display(){
+      var paperpos = this.body.position;
+      push();
+      ellipseMode(RADIUS);
+      fill("purple");
+      ellipse(paperpos.x, paperpos.y, this.radius, this.radius);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
+    }
+  }
